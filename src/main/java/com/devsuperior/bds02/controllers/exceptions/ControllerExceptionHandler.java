9252e295ac5bad54ce1106115e84c2b71ca5cf24
Douglas Monteles <silvadouglas328@gmail.com>
2022-04-16 13:25:00 -0300

    deleteShouldReturnNotFoundWhenNonExistingId passing

---
+++ src/main/java/com/devsuperior/bds02/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.devsuperior.bds02.controllers.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFoundException(ResourceNotFoundException e, 
+			HttpServletRequest request) {
+		var error = new StandardError();
+		
+		error.setTimestamp(Instant.now());
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setError(e.getMessage());
+		error.setMessage("Resource not found.");
+		error.setPath(request.getPathInfo());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	
+	
+}
