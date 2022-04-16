a0dff283e538823bdec34acdbcdde8bf09f4e34a
Douglas Monteles <silvadouglas328@gmail.com>
2022-04-16 13:07:48 -0300

    deleteShouldReturnNoContentWhenIndependentId passing

--- src/main/java/com/devsuperior/bds02/controllers/CityController.java
+++ src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +41,12 @@ public class CityController {
 				.toUri();
 		
 		return ResponseEntity.created(uri).body(newCityDTO);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		this.cityService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
