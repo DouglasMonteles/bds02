d21716d571611fd94aa96b66772525d902b3a53e
Douglas Monteles <silvadouglas328@gmail.com>
2022-04-16 13:25:39 -0300

    deleteShouldReturnBadRequestWhenDependentId passing

--- src/main/java/com/devsuperior/bds02/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/bds02/controllers/exceptions/ControllerExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
 import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -28,6 +29,18 @@ public class ControllerExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
 	
-	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> resourceNotFoundException(DatabaseException e, 
+			HttpServletRequest request) {
+		var error = new StandardError();
+		
+		error.setTimestamp(Instant.now());
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setError(e.getMessage());
+		error.setMessage("Resource not found.");
+		error.setPath(request.getPathInfo());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
 	
 }
